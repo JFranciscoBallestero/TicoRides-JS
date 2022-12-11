c71155b5f74f75ea490a62cc367dc4a7c1047a76
JFranciscoBallestero <franciscoballesterorivera@gmail.com>
2022-12-11 02:13:08 -0600

    Update main,js & login.html
    Finished the login function, fixed some details from the register function and updated login.html to implement JS.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
 $(function(){
+    
     /*Fuction used to register a user*/ 
     $('#register').click(function register(){    
         const frtName = document.getElementById('firstName').value;
         const lstName = document.getElementById('lastName').value;
         const number = document.getElementById('phone').value;
         const usName = document.getElementById('username').value;
-        const pass1 = document.getElementById('userPass1').value;
-        const pass2 = document.getElementById('userPass2').value;
+        const pass = document.getElementById('userPass1').value;
+        const passVerf = document.getElementById('userPass2').value;
         const speed = '';
         const about = '';
-        let data = {frtName, lstName, number, usName, pass1, speed, about};
-        if (frtName === '' || lstName === '' || number === '' || usName === '' || pass1 === '' ||
-            pass2 === ''){
+        const data = {frtName, lstName, number, usName, pass, speed, about};
+        if (frtName == '' || lstName == '' || number == '' || usName == '' || pass == '' ||
+            passVerf == ''){
             alert('Please fill every space.');
         }
         else{
-            if (pass1 != pass2){
+            if (pass != passVerf){
                 alert('The password needs to be the same');
             }
-            else if (pass1 === pass2){
+            else if (pass == passVerf){
                 if (localStorage.getItem('users') === null){
-                    let newUser = [data];
+                    const newUser = [data];
                     saveToLocalStorage('users', newUser);
                     saveToLocalStorage('currentUser', newUser);
                     window.location.href = 'dashboard.html';
-
                 }
                 else{
                     const userArray = JSON.parse(localStorage.getItem('users'));
@@ -33,17 +33,38 @@ $(function(){
                         alert("The username is already in use.");
                     }
                     else{
-                        let newUser = [data];
+                        const newUser = [data];
                         userArray.push(data);
                         saveToLocalStorage('users', userArray);
                         saveToLocalStorage('currentUser', newUser);
                         window.location.href = 'dashboard.html';
                     }
-                    
                 }
             }
         }
-        
+    })
+    /*Fuction used to login into the website*/
+    $('#login').click(function login(){
+        const user = document.getElementById('username').value;
+        const password = document.getElementById('userPass').value;    
+        if (user == '' || password == ''){
+            alert('Please fill every space.');
+        }
+        else{
+            if (localStorage.getItem('users') === null){
+                alert('There are no users registered yet. Please register first.');
+            }
+            else{
+                const userArray = JSON.parse(localStorage.getItem('users'));
+                if(userArray.some((x)=>{return x.usName==user && x.pass==password}))
+                {
+                    const index = userArray.findIndex(({usName}) => usName == user);
+                    const current = [userArray[index]];
+                    saveToLocalStorage('currentUser', current);
+                    window.location.href = 'dashboard.html';
+                }
+            }
+        }
     })
     function saveToLocalStorage(key, value) {
         localStorage.setItem(key, JSON.stringify(value));
