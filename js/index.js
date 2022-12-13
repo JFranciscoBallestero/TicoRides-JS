fd22517d55ef297dec4581d4bbb1b6e68921a151
JFranciscoBallestero <franciscoballesterorivera@gmail.com>
2022-12-13 02:57:03 -0600

    Updates index.js
    Finished the functions in index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,62 @@
 $(function(){
     localStorage.setItem('currentRide', JSON.stringify([]));
-
+    $('#search').click(function(){
+        const from = document.getElementById('from').value;
+        const to = document.getElementById('to').value;
+        if (from == "" && to== ""){
+            alert('Please fill one or both spaces to search for a ride.');
+            const rides = JSON.parse(localStorage.getItem('rides'));
+            const table = document.getElementById('ridesTable');
+            if (rides) {
+                let rows = "";
+                rides.forEach((ride, index) => {
+                    let row = `<tr>`;
+                    row += `<td>${ride.user}</td>`;
+                    row += `<td>${ride.start}</td>`;
+                    row += `<td>${ride.end}</td>`;
+                    row += `<td> <a href="#" onclick="view('${index}')">View</a></td>`;
+                    rows += row + "</tr>";
+                });
+                table.innerHTML = rows;
+            }
+        }
+        else if (from != ""){
+            const rides = JSON.parse(localStorage.getItem('rides'));
+            const table = document.getElementById('ridesTable');
+            if (rides) {
+                let rows = "";
+                rides.forEach((ride, index) => {
+                    if (ride.start == from){
+                        let row = `<tr>`;
+                        row += `<td>${ride.user}</td>`;
+                        row += `<td>${ride.start}</td>`;
+                        row += `<td>${ride.end}</td>`;
+                        row += `<td> <a href="#" onclick="view('${index}')">View</a></td>`;
+                        rows += row + "</tr>";
+                    }
+                });
+                table.innerHTML = rows;
+            }
+        }
+        else if (to != ""){
+            const rides = JSON.parse(localStorage.getItem('rides'));
+            const table = document.getElementById('ridesTable');
+            if (rides) {
+                let rows = "";
+                rides.forEach((ride, index) => {
+                    if (ride.end == to){
+                        let row = `<tr>`;
+                        row += `<td>${ride.user}</td>`;
+                        row += `<td>${ride.start}</td>`;
+                        row += `<td>${ride.end}</td>`;
+                        row += `<td> <a href="#" onclick="view('${index}')">View</a></td>`;
+                        rows += row + "</tr>";
+                    }
+                });
+                table.innerHTML = rows;
+            }
+        }
+    })
         /*Generates the table with rides that were created by the user*/
         function createTable(){
             const rides = JSON.parse(localStorage.getItem('rides'));
@@ -16,7 +72,7 @@ $(function(){
                     rows += row + "</tr>";
                 });
                 table.innerHTML = rows;
-          }
+            }
         }
         createTable();
 })
